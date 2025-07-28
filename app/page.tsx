0b7429d48a3581146abd3bab6519b62d759b1c71
Ken Irwin <ken@kenirwin.net>
2025-07-28 14:47:29 -0400

    validate form input

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Guest from '@/components/Guest';
 import { currentUser } from '@clerk/nextjs/server';
+import AddTransaction from '@/components/AddTransaction';
 
 const Home = async () => {
   const user = await currentUser();
@@ -10,6 +11,7 @@ const Home = async () => {
   return (
     <main>
       <h1>Welcome, {user.firstName} </h1>
+      <AddTransaction />
     </main>
   );
 };
