0c0794e10fef73de5a4ae055edeeeb9c1ded14ce
Ken Irwin <ken@kenirwin.net>
2025-07-29 10:06:20 -0400

    show user balance

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -1,35 +1,35 @@
-import { currentUser } from "@clerk/nextjs/server";
-import { db } from '@/lib/db'
+import { currentUser } from '@clerk/nextjs/server';
+import { db } from '@/lib/db';
 
 export const checkUser = async () => {
-    const user = await currentUser();
-    console.log(user)
-    //check for current logged in clerk user
-    if (!user) {
-        return null;
-    }
+  const user = await currentUser();
+  // console.log(user);
+  //check for current logged in clerk user
+  if (!user) {
+    return null;
+  }
 
-    // check if user is already in the database
-    const loggedInUser = await db.user.findUnique({
-        where: {
-            clerkUserId: user.id
-        }
-    });
+  // check if user is already in the database
+  const loggedInUser = await db.user.findUnique({
+    where: {
+      clerkUserId: user.id,
+    },
+  });
 
-    // if user is in the database, return user
-    if (loggedInUser) {
-        return loggedInUser;
-    }
+  // if user is in the database, return user
+  if (loggedInUser) {
+    return loggedInUser;
+  }
 
-    // if not in the database, create user
-    const newUser = await db.user.create({
-       data: {
-        clerkUserId: user.id,
-        name: `${user.firstName} ${user.lastName}`,
-        imageUrl: user.imageUrl,
-        email: user.emailAddresses[0].emailAddress
-       }
-    })
+  // if not in the database, create user
+  const newUser = await db.user.create({
+    data: {
+      clerkUserId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
 
-    return newUser;
-}
+  return newUser;
+};
