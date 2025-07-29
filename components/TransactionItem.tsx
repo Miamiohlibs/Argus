e01b3de00bd3f734b253440068a80c548d7359ab
Ken Irwin <ken@kenirwin.net>
2025-07-29 11:40:57 -0400

    add delete transaction functionality

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -1,9 +1,22 @@
+'use client';
 import { Transaction } from '@/types/Transaction';
 import { addCommas } from '@/lib/utils';
 import { toast } from 'react-toastify';
+import deleteTransaction from '@/app/actions/deleteTransaction';
 
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+';
+  const handleDeleteTransaction = async (transactionId: string) => {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete this transaction?'
+    );
+    if (!confirmed) return;
+    const { message, error } = await deleteTransaction(transactionId);
+    if (error) {
+      toast.error(error);
+    }
+    toast.success(message);
+  };
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}
@@ -11,6 +24,12 @@ const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
         {sign}
         {addCommas(Math.abs(transaction.amount))}
       </span>
+      <button
+        onClick={() => handleDeleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 };
