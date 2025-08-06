6fac85e205672764923961e4798b6f715b112e37
Ken Irwin <ken@kenirwin.net>
2025-08-06 11:17:42 -0400

    add admin nav component, checks for admin access

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,17 +6,18 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs';
+import AdminNav from './AdminNav';
 import { checkUser } from '@/lib/checkUser';
 
 const Header = async () => {
   const user = await checkUser();
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <NavbarBrand href="/" className="ml-3">
-        Watson/Argus
-      </NavbarBrand>
-      <Nav className="ms-auto mr-3">
+    <Navbar bg="dark" variant="dark" expand="lg" className="px-2">
+      <NavbarBrand href="/">Watson/Argus</NavbarBrand>
+      <Nav className="ms-auto me-3 text-light">
+        <AdminNav />
+        <NavItem className="pe-5">Test</NavItem>
         <SignedOut>
           <NavItem>
             <SignInButton />
