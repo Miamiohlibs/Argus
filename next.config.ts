423d2edd925b170890192d53488733cdbf5040aa
Rachel Maderik <maderir@ulblwebp21.lib.miamioh.edu>
2025-08-15 14:39:31 -0400

    Configuration for serving as subdirectory

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/watson',
+  assetPrefix: '/watson/',
+  trailingSlash: true
 };
 
 export default nextConfig;
