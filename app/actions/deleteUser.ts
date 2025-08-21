a0da3b2a9810070628403c61e79a15295c447416
Ken Irwin <ken@kenirwin.net>
2025-08-21 11:17:01 -0400

    prevent admin from deleting superadmin user

--- app/actions/deleteUser.ts
+++ app/actions/deleteUser.ts
@@ -1,7 +1,7 @@
 'use server';
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs/server';
-import { isAdmin } from '@/lib/canEdit';
+import { isAdmin, isSuperAdmin } from '@/lib/canEdit';
 
 async function deleteUser(userIdToDelete: string): Promise<{
   message?: string;
@@ -13,19 +13,35 @@ async function deleteUser(userIdToDelete: string): Promise<{
   }
   console.log(`deletion request for user ${userIdToDelete} by ${userId}`);
   const canDelete = await isAdmin();
+  const canDeleteSuperAdmin = await isSuperAdmin();
   if (!canDelete) {
     console.log(
       `user deletion permission denied on ${userIdToDelete} by ${userId}`
     );
     return { error: 'Delete user permission denied' };
   }
+
+  // Array of AND conditions for Prisma where query on db.user.deleteMany.
+  // Each object can include any fields from the User type.
+  const deleteRequirementsArray: Array<Record<string, any>> = [
+    { id: userIdToDelete }, // id to delete
+    { clerkUserId: { not: userId } }, // don't delete yourself
+  ];
+  if (!canDeleteSuperAdmin) {
+    // admin cannot delete superadmin
+    deleteRequirementsArray.push({ role: { not: 'superadmin' } });
+  }
+  console.log(
+    'attempting to delete with requirements:',
+    deleteRequirementsArray
+  );
   try {
     // this is a bit of a hack: we're not really going to deleteMany
     // only one or zero users will match
     // but deleteMany supports the syntax to prevent the user from deleting themself
     const deletedUsers = await db.user.deleteMany({
       where: {
-        AND: [{ id: userIdToDelete }, { clerkUserId: { not: userId } }],
+        AND: deleteRequirementsArray,
       },
     });
     if (deletedUsers.count == 0) {
