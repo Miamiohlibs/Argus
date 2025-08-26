d853be89cc02c920ea0a92e9eb9d19e17a83d497
Ken Irwin <ken@kenirwin.net>
2025-08-26 09:37:29 -0400

    make "my projects" specific to editors

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,23 +7,26 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs';
-import AdminNav from './AdminNav';
 import { checkUser } from '@/lib/checkUser';
+import NavEditor from './NavEditor';
+import NavAdmin from './NavAdmin';
 
 const Header = async () => {
   const user = await checkUser();
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="px-2">
-      <NavbarBrand as={NextLink} href="/">Watson/Argus</NavbarBrand>
+      <NavbarBrand as={NextLink} href="/">
+        Watson/Argus
+      </NavbarBrand>
       <Nav className="ms-auto me-3 text-light">
+        <NavEditor />
         <NavItem>
-          <NavLink as={NextLink} href="/">My Projects</NavLink>
+          <NavLink as={NextLink} href="/allProjects">
+            All Projects
+          </NavLink>
         </NavItem>
-        <NavItem>
-          <NavLink as={NextLink} href="/allProjects">All Projects</NavLink>
-        </NavItem>
-        <AdminNav />
+        <NavAdmin />
         <SignedOut>
           <NavItem>
             <SignInButton />
