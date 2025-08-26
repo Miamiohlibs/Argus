92006abde2f83a4e183357d3784ebe4544fd5341
Ken Irwin <ken@kenirwin.net>
2025-08-26 09:50:57 -0400

    remove duplicate routes

--- app/demo/page.tsx
+++
@@ -1,9 +0,0 @@
-// import ArchivesRequestSlip from '@/components/ArchivesRequestSlip';
-import RequestSlip from './RequestSlip';
-export default function Demo() {
-  return (
-    <>
-      <RequestSlip />
-    </>
-  );
-}
