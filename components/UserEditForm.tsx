7683da9e383fce149185a10c65897e102bdb3233
Ken Irwin <ken@kenirwin.net>
2025-08-27 11:51:27 -0400

    add Status in UserEditForm

--- components/UserEditForm.tsx
+++ components/UserEditForm.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import updateUser from '@/app/actions/updateUser';
 import { toast } from 'react-toastify';
-
+import { User, Role, UserAffiliation, UserStatus } from '@prisma/client';
 import {
   Form,
   FormLabel,
@@ -12,45 +12,77 @@ import {
 } from 'react-bootstrap';
 
 // import { User } from '@/types/User';
-import { User } from '@prisma/client';
 // import { revalidatePath } from 'next/cache';
+
 export default function UserEditForm({ user }: { user: User }) {
   const [role, setRole] = useState(user.role);
-  const validRoles = ['user', 'editor', 'admin', 'superadmin'] as const;
-  type Role = (typeof validRoles)[number];
+  const validRoles = Object.values(Role);
+  // type Role = (typeof validRoles)[number];
+  const [status, setStatus] = useState(user.status);
+  const validStatuses = Object.values(UserStatus);
+  // type Role = (typeof validRoles)[number];
 
-  // const isValidRole = (role: string): role is Role => {
-  //   return validRoles.includes(role as Role);
-  // };
-
-  const handleChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setRole(e.target.value as Role);
-  };
+  const handleChange =
+    (targetField: 'role' | 'status') =>
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      console.log(`changing target: ${targetField}, ${e.target.value}`);
+      switch (targetField) {
+        case 'role':
+          setRole(e.target.value as Role);
+          break;
+        case 'status':
+          setStatus(e.target.value as UserStatus);
+          break;
+      }
+    };
 
   const handleSubmit = async (e: React.FormEvent) => {
-    // if (isValidRole(role)) {
     e.preventDefault();
-    const updatedUser = await updateUser(user.id, { role: role as Role });
+    const updatedUser = await updateUser(user.id, {
+      role: role as Role,
+      status: status as UserStatus,
+    });
     if (updatedUser.error) {
       console.error('Error updating user:', updatedUser.error);
       return;
     }
     toast.success('User role updated successfully');
-    // } else {
-    //   console.error('Invalid role: ', role)
-    // }
   };
 
+  let statusPulldown = validStatuses.map((r) => (
+    <option key={r} value={r}>
+      {r.charAt(0).toUpperCase() + r.slice(1)}
+    </option>
+  ));
+  statusPulldown.unshift(
+    <option key="none" value="">
+      None
+    </option>
+  );
   return (
     <Form onSubmit={handleSubmit}>
       <InputGroup>
         <FormLabel htmlFor="role">Role</FormLabel>
-        <FormSelect id="role" value={role} onChange={handleChange}>
+        <FormSelect
+          id="role"
+          value={role ?? ''}
+          onChange={(e) => handleChange('role')(e)}
+        >
           {validRoles.map((r) => (
             <option key={r} value={r}>
               {r.charAt(0).toUpperCase() + r.slice(1)}
             </option>
           ))}
+        </FormSelect>
+      </InputGroup>
+      <InputGroup>
+        <FormLabel htmlFor="status">Status</FormLabel>
+        <FormSelect
+          id="status"
+          value={status ?? ''}
+          onChange={handleChange('status')}
+        >
+          {statusPulldown}
         </FormSelect>
       </InputGroup>
       <Button className="btn btn-primary" type="submit">
