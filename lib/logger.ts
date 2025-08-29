eb701e758a8867a55cc03cfbadef8450d796997f
Ken Irwin <ken@kenirwin.net>
2025-08-29 12:35:49 -0400

    winston logs to logfiles

--- lib/logger.ts
+++ lib/logger.ts
@@ -3,27 +3,36 @@ import 'winston-daily-rotate-file';
 const { combine, timestamp, json, errors } = winston.format;
 import path from 'path';
 
-// const logDirectory = path.join(__dirname);
-// console.log('***logdirectory', logDirectory);
-// // Ensure the log directory exists (optional, but good practice)
-// import fs from 'fs';
-// if (!fs.existsSync(logDirectory)) {
-//   fs.mkdirSync(logDirectory);
-// }
+const logDirectory = path.join(process.cwd(), 'logs');
+// console.log('***', logDirectory);
 
-const fileRotateTransport = new winston.transports.DailyRotateFile({
-  filename: 'combined-%DATE%.log',
+// Ensure the log directory exists (optional, but good practice)
+import fs from 'fs';
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+
+const combinedLogTransport = new winston.transports.DailyRotateFile({
+  filename: path.join(logDirectory, 'combined-%DATE%.log'),
   datePattern: 'YYYY-MM-DD',
   maxFiles: '14d',
+  format: combine(errors({ stack: true }), timestamp(), json()),
+});
+
+const errorLogTransport = new winston.transports.DailyRotateFile({
+  filename: path.join(logDirectory, 'combined-%DATE%.log'),
+  datePattern: 'YYYY-MM-DD',
+  maxFiles: '14d',
+  format: combine(errors({ stack: true }), timestamp(), json()),
+  level: 'error',
 });
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: combine(timestamp(), json()),
   transports: [
-    new winston.transports.File({
-      filename: 'combined.log',
-    }),
+    combinedLogTransport,
+    errorLogTransport,
     new winston.transports.Console({
       level: 'silly',
     }),
