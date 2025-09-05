c98b20bcee94f4a609eb33c47d4a987198ed94b8
Ken Irwin <ken@kenirwin.net>
2025-09-05 14:55:15 -0400

    testApi gets multiple holdings

--- app/api/testApi/route.tsx
+++ app/api/testApi/route.tsx
@@ -3,6 +3,17 @@ import { bibHoldingsByAny } from '@/app/actions/almaSearch';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const response = await bibHoldingsByAny('Green eggs and ham');
+  const arr = [
+    // '991000014819708518', //Flosculi
+    'Z250.3 .F58 1967', // Flosculi
+    'PS3552.R4825 E27 1990', // Earth
+    // '991017357419708518', // Earth
+    '991000024539708518', // Orwell
+  ];
+  const response = await Promise.all(
+    arr.map(async (item) => {
+      return await bibHoldingsByAny(item);
+    })
+  );
   return NextResponse.json(response);
 }
