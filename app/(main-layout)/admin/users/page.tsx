6c9d0d385c9cbf20662ecfaa31c9dbd324ede857
Ken Irwin <ken@kenirwin.net>
2025-09-10 15:34:17 -0400

    accessibility fixes for UserTable and page

--- app/(main-layout)/admin/users/page.tsx
+++ app/(main-layout)/admin/users/page.tsx
@@ -14,7 +14,7 @@ export default async function UsersPage() {
   }
   return (
     <main className="p-4">
-      <h1 className="text-xl font-bold mb-4">User List</h1>
+      <h1 className="text-xl font-bold mb-4">Manage Users</h1>
       <UserTableWrapper user={user} canDeleteSuperAdmin={canDeleteSuperAdmin} />
     </main>
   );
