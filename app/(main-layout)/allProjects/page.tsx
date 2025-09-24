6834d3bc3a41dcb79701fef096780862d899b95b
Ken Irwin <ken@kenirwin.net>
2025-09-24 17:00:15 -0400

    add canPrint on my projects & all projects

--- app/(main-layout)/allProjects/page.tsx
+++ app/(main-layout)/allProjects/page.tsx
@@ -2,19 +2,24 @@ import ProjectsTable from '@/components/ProjectsTable';
 import CreateProjectButton from '@/components/CreateProjectButton';
 // import { currentUser } from '@clerk/nextjs/server';
 import { checkUser } from '@/lib/checkUser';
-import { isEditorOrAbove } from '@/lib/canEdit';
+import { isEditorOrAbove, canPrint } from '@/lib/canEdit';
 
 const AllProjectsPage = async () => {
   // const clerkUser = (await currentUser()) || null;
   // const userId = clerkUser ? clerkUser.id || null : null;
   const user = await checkUser();
   const isEditor = await isEditorOrAbove();
+  const canPrintBool = (await canPrint()) ?? false;
 
   return (
     <>
       <h1 className="h2">All Projects</h1>
       {isEditor && <CreateProjectButton />}
-      <ProjectsTable limitToUser={false} user={user} />
+      <ProjectsTable
+        limitToUser={false}
+        user={user}
+        canPrintBool={canPrintBool}
+      />
     </>
   );
 };
--- app/(main-layout)/page.tsx
+++ app/(main-layout)/page.tsx
@@ -3,13 +3,14 @@ import { currentUser } from '@clerk/nextjs/server';
 import ProjectsTable from '@/components/ProjectsTable';
 import CreateProjectButton from '@/components/CreateProjectButton';
 import { checkUser } from '@/lib/checkUser';
-import { isEditorOrAbove } from '@/lib/canEdit';
+import { isEditorOrAbove, canPrint } from '@/lib/canEdit';
 import { redirect } from 'next/navigation';
 
 const Home = async () => {
   const user = await checkUser();
   const clerkUserInfo = (await currentUser()) ?? { firstName: 'Guest' };
   const isEditor = await isEditorOrAbove();
+  const canPrintBool = (await canPrint()) ?? false;
 
   if (!user) {
     return <Guest />;
@@ -21,7 +22,11 @@ const Home = async () => {
     <>
       <h1 className="h2">{clerkUserInfo.firstName}&apos;s Projects</h1>
       <div className="mb-3">{isEditor && <CreateProjectButton />}</div>
-      <ProjectsTable limitToUser={true} user={user} />
+      <ProjectsTable
+        limitToUser={true}
+        user={user}
+        canPrintBool={canPrintBool}
+      />
     </>
   );
 };
