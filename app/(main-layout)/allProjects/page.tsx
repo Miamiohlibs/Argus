ec851faf249dc576e2614c737dade46601c8c6b2
Ken Irwin <ken@kenirwin.net>
2025-09-25 15:53:17 -0400

    remove unused code on allProjects

--- app/(main-layout)/allProjects/page.tsx
+++ app/(main-layout)/allProjects/page.tsx
@@ -1,15 +1,9 @@
 import ProjectsTable from '@/components/ProjectsTable';
 import CreateProjectButton from '@/components/CreateProjectButton';
-// import { currentUser } from '@clerk/nextjs/server';
-import { checkUser } from '@/lib/checkUser';
-import { isEditorOrAbove, canPrint } from '@/lib/canEdit';
 import getUserInfo from '@/lib/getUserInfo';
 
 const AllProjectsPage = async () => {
-  // const clerkUser = (await currentUser()) || null;
-  // const userId = clerkUser ? clerkUser.id || null : null;
   const { user, permissions } = await getUserInfo();
-  // const user = await checkUser();
   const isEditor = permissions.isEditorOrAbove;
   const canPrint = permissions.canPrint;
 
