99baa2a8f3771a7e03c6de11347ea5f7dc2842a3
Ken Irwin <ken@kenirwin.net>
2025-09-25 16:46:03 -0400

    simplify permissions on allProjects

--- app/(main-layout)/allProjects/page.tsx
+++ app/(main-layout)/allProjects/page.tsx
@@ -3,14 +3,15 @@ import CreateProjectButton from '@/components/CreateProjectButton';
 import getUserInfo from '@/lib/getUserInfo';
 
 const AllProjectsPage = async () => {
-  const { user, permissions } = await getUserInfo();
-  const isEditor = permissions.isEditorOrAbove;
-  const canPrint = permissions.canPrint;
+  const {
+    user,
+    permissions: { isEditorOrAbove, canPrint },
+  } = await getUserInfo();
 
   return (
     <>
       <h1 className="h2">All Projects</h1>
-      {isEditor && <CreateProjectButton />}
+      {isEditorOrAbove && <CreateProjectButton />}
       <ProjectsTable limitToUser={false} user={user} canPrintBool={canPrint} />
     </>
   );
