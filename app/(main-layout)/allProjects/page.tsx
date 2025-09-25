dda938d4c4ea4ffa5c4bd329e6591c591dbae0d7
Ken Irwin <ken@kenirwin.net>
2025-09-25 15:52:38 -0400

    use new permissions on allProjects

--- app/(main-layout)/allProjects/page.tsx
+++ app/(main-layout)/allProjects/page.tsx
@@ -3,23 +3,21 @@ import CreateProjectButton from '@/components/CreateProjectButton';
 // import { currentUser } from '@clerk/nextjs/server';
 import { checkUser } from '@/lib/checkUser';
 import { isEditorOrAbove, canPrint } from '@/lib/canEdit';
+import getUserInfo from '@/lib/getUserInfo';
 
 const AllProjectsPage = async () => {
   // const clerkUser = (await currentUser()) || null;
   // const userId = clerkUser ? clerkUser.id || null : null;
-  const user = await checkUser();
-  const isEditor = await isEditorOrAbove();
-  const canPrintBool = (await canPrint()) ?? false;
+  const { user, permissions } = await getUserInfo();
+  // const user = await checkUser();
+  const isEditor = permissions.isEditorOrAbove;
+  const canPrint = permissions.canPrint;
 
   return (
     <>
       <h1 className="h2">All Projects</h1>
       {isEditor && <CreateProjectButton />}
-      <ProjectsTable
-        limitToUser={false}
-        user={user}
-        canPrintBool={canPrintBool}
-      />
+      <ProjectsTable limitToUser={false} user={user} canPrintBool={canPrint} />
     </>
   );
 };
