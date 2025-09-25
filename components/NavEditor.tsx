94598d3cb72fe77975b06a9042bfa4f53cdca51b
Ken Irwin <ken@kenirwin.net>
2025-09-25 17:47:32 -0400

    tidy NavEditor file of old code

--- components/NavEditor.tsx
+++ components/NavEditor.tsx
@@ -1,18 +1,16 @@
 import logger from '@/lib/logger';
 import NextLink from 'next/link';
 import { NavItem, NavLink } from 'react-bootstrap';
-// import checkAccess from '@/lib/checkAccess';
-// import { isEditorOrAbove } from '@/lib/canEdit';
 import getUserInfo from '@/lib/getUserInfo';
 const NavEditor = async () => {
-  // Check if the user has access to admin features
+  // Check if the user has access to editor features
   const {
     permissions: { isEditorOrAbove },
   } = await getUserInfo();
 
   logger.verbose('NavEditor - hasAccess:', isEditorOrAbove);
   if (!isEditorOrAbove) {
-    return null; // If no access, do not render the admin navigation
+    return null; // If no access, do not render the editor navigation
   }
   return (
     <>
