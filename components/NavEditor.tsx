0c7d77fbc7c428f1ebf3db5a199691f1b4723226
Ken Irwin <ken@kenirwin.net>
2025-09-25 17:46:34 -0400

    use new permissions in NavEditor

--- components/NavEditor.tsx
+++ components/NavEditor.tsx
@@ -2,23 +2,25 @@ import logger from '@/lib/logger';
 import NextLink from 'next/link';
 import { NavItem, NavLink } from 'react-bootstrap';
 // import checkAccess from '@/lib/checkAccess';
-import { isEditorOrAbove } from '@/lib/canEdit';
+// import { isEditorOrAbove } from '@/lib/canEdit';
+import getUserInfo from '@/lib/getUserInfo';
 const NavEditor = async () => {
   // Check if the user has access to admin features
-  const hasAccess = await isEditorOrAbove();
-  logger.verbose('NavEditor - hasAccess:', hasAccess);
-  if (!hasAccess) {
+  const {
+    permissions: { isEditorOrAbove },
+  } = await getUserInfo();
+
+  logger.verbose('NavEditor - hasAccess:', isEditorOrAbove);
+  if (!isEditorOrAbove) {
     return null; // If no access, do not render the admin navigation
   }
   return (
     <>
-      {/* <Nav className="me-3"> */}
       <NavItem>
         <NavLink as={NextLink} href="/">
           My Projects
         </NavLink>
       </NavItem>
-      {/* </Nav> */}
     </>
   );
 };
