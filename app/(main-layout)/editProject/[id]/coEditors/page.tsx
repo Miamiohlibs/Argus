8338db88a17da871f2bcf3fe8b9134248e1245f3
Ken Irwin <ken@kenirwin.net>
2025-10-14 10:23:02 -0400

    show ProjectMetadata on coeditor page

--- app/(main-layout)/editProject/[id]/coEditors/page.tsx
+++ app/(main-layout)/editProject/[id]/coEditors/page.tsx
@@ -2,15 +2,26 @@ import { checkUser } from '@/lib/checkUser';
 import { getProject } from '@/app/actions/projectActions';
 import { getPossibleCoEditors } from '@/app/actions/coEditors';
 import CoEditorTable from '@/components/CoEditorTable';
+import ProjectMetadata from '@/components/ProjectMetadata';
+import { notFound } from 'next/navigation';
 
 interface EditProjectPageProps {
   params: Promise<{ id: string }>;
 }
 export default async function CoEditorPage({ params }: EditProjectPageProps) {
-  const currentUser = await checkUser();
+  // const currentUser = await checkUser();
   const { id } = await params;
-  //   const response = await getProject({ id });
+  const { project } = await getProject({ id });
   //   const response = await getPossibleCoEditors(id);
   //   return <pre>{JSON.stringify(response, null, 2)}</pre>;
-  return <CoEditorTable projectId={id} />;
+  if (!project || project == null) {
+    notFound();
+  }
+  return (
+    <>
+      <h1 className="h2">Add Co-Editor(s) on: {project?.title}</h1>
+      <ProjectMetadata project={project} />
+      <CoEditorTable projectId={id} />
+    </>
+  );
 }
