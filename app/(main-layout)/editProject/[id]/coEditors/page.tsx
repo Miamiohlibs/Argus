9a760abeff98452fb736a5791d65314add5d9e15
Ken Irwin <ken@kenirwin.net>
2025-10-14 15:52:40 -0400

    restrict editProject/id/coEditors to admins and owners

--- app/(main-layout)/editProject/[id]/coEditors/page.tsx
+++ app/(main-layout)/editProject/[id]/coEditors/page.tsx
@@ -1,21 +1,25 @@
-import { checkUser } from '@/lib/checkUser';
 import { getProject } from '@/app/actions/projectActions';
-// import { getPossibleCoEditors } from '@/app/actions/coEditors';
 import CoEditorTable from '@/components/CoEditorTable';
 import ProjectMetadata from '@/components/ProjectMetadata';
 import { notFound } from 'next/navigation';
 import RemoveCoEditorButton from '@/components/RemoveCoEditorButton';
 import ProjectButtons from '@/components/ProjectButtons';
+import getUserInfo from '@/lib/getUserInfo';
+import { unauthorized } from 'next/navigation';
 
 interface EditProjectPageProps {
   params: Promise<{ id: string }>;
 }
 export default async function CoEditorPage({ params }: EditProjectPageProps) {
-  // const currentUser = await checkUser();
   const { id } = await params;
+  const {
+    permissions: { isOwner, isAdmin },
+  } = await getUserInfo(id);
+  if (!(isOwner || isAdmin)) {
+    unauthorized();
+  }
+
   const { project } = await getProject({ id });
-  //   const response = await getPossibleCoEditors(id);
-  //   return <pre>{JSON.stringify(response, null, 2)}</pre>;
   if (!project || project == null) {
     notFound();
   }
