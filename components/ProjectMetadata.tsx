f3bad9ce176d7111e8d0a0415f1da4c890c0d516
Ken Irwin <ken@kenirwin.net>
2025-10-14 11:26:48 -0400

    display co-editors in projectMetadata

--- components/ProjectMetadata.tsx
+++ components/ProjectMetadata.tsx
@@ -1,13 +1,20 @@
 import { ProjectWithUserAndBib } from '@/types/ProjectWithUserAndBib';
+import { Badge } from 'react-bootstrap';
 
 const ProjectMetadata = ({ project }: { project: ProjectWithUserAndBib }) => {
   return (
     <>
       <div className="mb-3 text-muted small">
         <span className="fw-bold">{project?.title}</span>
-        <span className="badge bg-secondary ms-2">
+        <Badge bg="secondary" className="ms-2">
           Owner: {project?.user.name}
-        </span>
+        </Badge>
+        {project.coEditors &&
+          project.coEditors.map((ed) => (
+            <Badge bg="light" className="ms-2 text-dark" key={ed.id}>
+              Co-Editor: {ed.name}
+            </Badge>
+          ))}
       </div>
     </>
   );
