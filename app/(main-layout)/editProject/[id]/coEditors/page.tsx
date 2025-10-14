7cb9b5bb32b4124403a7a8d02d31e5c6e9fb7458
Ken Irwin <ken@kenirwin.net>
2025-10-14 15:06:07 -0400

    add ProjectButtons to coEditor page

--- app/(main-layout)/editProject/[id]/coEditors/page.tsx
+++ app/(main-layout)/editProject/[id]/coEditors/page.tsx
@@ -1,10 +1,11 @@
 import { checkUser } from '@/lib/checkUser';
 import { getProject } from '@/app/actions/projectActions';
-import { getPossibleCoEditors } from '@/app/actions/coEditors';
+// import { getPossibleCoEditors } from '@/app/actions/coEditors';
 import CoEditorTable from '@/components/CoEditorTable';
 import ProjectMetadata from '@/components/ProjectMetadata';
 import { notFound } from 'next/navigation';
 import RemoveCoEditorButton from '@/components/RemoveCoEditorButton';
+import ProjectButtons from '@/components/ProjectButtons';
 
 interface EditProjectPageProps {
   params: Promise<{ id: string }>;
@@ -22,9 +23,15 @@ export default async function CoEditorPage({ params }: EditProjectPageProps) {
     <>
       <h1 className="h2">Add Co-Editor(s) on: {project?.title}</h1>
       <ProjectMetadata project={project} />
+      <ProjectButtons
+        projectId={project.id}
+        canEdit={false}
+        canPrint={false}
+        onPage="Co-Editors"
+      />
       {project.coEditors.length > 0 && (
         <>
-          <h2 className="h2">Current Co-Editors</h2>
+          <h2 className="h2 mt-3">Current Co-Editors</h2>
           <ul>
             {project.coEditors.map((ed) => {
               return (
