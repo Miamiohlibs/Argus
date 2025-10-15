90001a70d67cc9de68dccd2e8d2e241d43db5442
Ken Irwin <ken@kenirwin.net>
2025-10-15 15:21:44 -0400

    add page metadata for printSlips page

--- app/(no-layout)/slips/[id]/page.tsx
+++ app/(no-layout)/slips/[id]/page.tsx
@@ -1,7 +1,15 @@
 'use server';
+import { Metadata } from 'next';
 import MultiPagePdf from './ClientMultipagePdf';
 import { unauthorized } from 'next/navigation';
 import getUserInfo from '@/lib/getUserInfo';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'Print Slips | Argus',
+    description: 'Print PDF slips for each project item',
+  };
+}
 
 export default async function PdfWrapper() {
   const {
