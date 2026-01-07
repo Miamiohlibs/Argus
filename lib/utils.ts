005b763ddb7593953dc97dbaf7e1ff43820d8bad
Ken Irwin <ken@kenirwin.net>
2026-01-07 11:25:03 -0500

    add getProjectPurposes to lib/utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,27 @@
 export function addCommas(x: number): string {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
+export function getProjectPurposes(): string[] {
+  // get the list of allowed project purposes from .env
+  // if it includes "Other", remove "Other" from the list
+  // then alphabetize the list and append Other at the end
+  try {
+    if (process.env.SCHEMA_PROJECT_PURPOSES) {
+      const purposes = JSON.parse(process.env.SCHEMA_PROJECT_PURPOSES);
+      const otherIndex = purposes.indexOf('Other');
+      if (otherIndex >= 0) {
+        purposes.splice(otherIndex);
+      }
+      purposes.sort();
+      purposes.push('Other');
+      return purposes;
+    }
+    return ['Other'];
+  } catch (err) {
+    console.log(
+      `Error reading environment var SCHEMA_PROJECT_PURPOSES; invalid JSON array? Error: ${err}`
+    );
+    return ['Other'];
+  }
+}
