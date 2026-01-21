57215be087c0dd74aeb436fdb5e7eaaac9681dc7
Ken Irwin <ken@kenirwin.net>
2026-01-21 16:39:01 -0500

    remove unused code from admin/quickslip/handler/page.tsx

--- app/(main-layout)/admin/quickSlip/handler/page.tsx
+++ app/(main-layout)/admin/quickSlip/handler/page.tsx
@@ -1,33 +1,6 @@
 import { RequestSlipProps } from '@/types/RequestSlipProps';
 import ClientQuickPdf from './ClientQuickPdf';
 
-// function assignRequestSlipProps(
-//   input: Record<string, unknown>
-// ): RequestSlipProps {
-//   const result: RequestSlipProps = { purpose: 'Quick Slip' };
-
-//   if (input.hasOwnProperty('author') && typeof input.author == 'string')
-//     result.author = input.author;
-//   if (input.hasOwnProperty('title') && typeof input.title == 'string')
-//     result.title = input.title;
-//   if (
-//     input.hasOwnProperty('call_number') &&
-//     typeof input.call_number == 'string'
-//   )
-//     result.callNumber = input.call_number;
-
-//   if (
-//     input.hasOwnProperty('publisher_const') &&
-//     typeof input.publisher_const == 'string'
-//   )
-//     result.publisher = input.publisher_const;
-
-//   if (input.hasOwnProperty('notes') && typeof input.notes == 'string')
-//     result.notes = input.notes;
-
-//   return result;
-// }
-
 interface PageProps {
   searchParams: Record<string, string | string[] | undefined>;
 }
