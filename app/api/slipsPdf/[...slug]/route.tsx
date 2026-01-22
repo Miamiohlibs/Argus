a1ed59b811e51074e8289ca2033e9f7224ff510d
Ken Irwin <ken@kenirwin.net>
2026-01-22 14:51:26 -0500

    move request slip generation logic from slips/[...slug]/route to lib/generateRequestSlipItems

--- app/api/slipsPdf/[...slug]/route.tsx
+++ app/api/slipsPdf/[...slug]/route.tsx
@@ -8,6 +8,7 @@ import getEntries from '@/app/actions/getEntries';
 import { getProject } from '@/app/actions/projectActions';
 import filenamify from 'filenamify';
 import { checkUser } from '@/lib/checkUser';
+import generateRequestSlipItems from '@/lib/generateRequestSlipItems';
 
 export async function GET(
   req: NextRequest,
@@ -21,98 +22,21 @@ export async function GET(
   const user = await checkUser();
 
   const entries = data?.entries ?? [];
-  // return one slip per item (or one per bib if there are no items)
-  const items: RequestSlipProps[] = entries.flatMap((entry) => {
-    // Build item info strings for all items
-    const itemInfos =
-      entry.items?.map((item) => {
-        let info: string = '';
 
-        // only add to item info if there's any item-specific info
-        if (
-          (item.copy_id && parseInt(item.copy_id) > 1) ||
-          (item.description && item.description.length > 0)
-        ) {
-          if (
-            item.location_code &&
-            entry.location_codes &&
-            entry.location_codes.length > 1
-          ) {
-            info += `${item.location_name} (${item.location_code}) `;
-          }
-          if (item.call_number !== entry.callNumber) {
-            info += `${item.call_number} `;
-          }
-          if (item.description) {
-            info += item.description;
-          }
-          if (item.copy_id && parseInt(item.copy_id) > 1) {
-            info += ` c.${item.copy_id}`;
-          }
-        }
-        return info.trim();
-      }) ?? [];
+  if (entries && project && user) {
+    const items = generateRequestSlipItems(entries, project, user);
 
-    if (itemInfos.length > 0) {
-      // One object per item, highlighting the index
-      const locationStr = `${entry.location_display} (${entry.location_codes})`;
-      return itemInfos.map((_, idx) => ({
-        author: entry.author,
-        title: entry.itemTitle,
-        callNumber: entry.callNumber ?? undefined,
-        notes: entry.notes ?? undefined,
-        date: entry.pub_date ?? undefined,
-        location: locationStr,
-        ms: entry.items[0].ms ?? undefined,
-        box: entry.items[0].box ?? undefined,
-        folder: entry.items[0].folder ?? undefined,
-        itemInfo: itemInfos, // full array of all items
-        highlightedItemIndex: idx, // which one is current
-        userName: project?.user.name,
-        userEmail: project?.user.email,
-        userAffiliation: project?.user.affiliation ?? undefined,
-        userStatus: project?.user.status ?? undefined,
-        personPrinting: user?.name ?? 'Unknown',
-        projectName: project?.title,
-        purpose: project?.purpose,
-      }));
-    } else {
-      // Fallback: no items, return a single object
-      return [
-        {
-          author: entry.author,
-          title: entry.itemTitle,
-          callNumber: entry.callNumber ?? undefined,
-          notes: entry.notes ?? undefined,
-          date: entry.pub_date ?? undefined,
-          location: `${entry.location_display} (${entry.location_codes})`,
-          box: '',
-          folder: '',
-          ms: '',
-          itemInfo: [],
-          highlightedItemIndex: 0,
-          userName: project?.user.name,
-          userEmail: project?.user.email,
-          userAffiliation: project?.user.affiliation ?? undefined,
-          userStatus: project?.user.status ?? undefined,
-          personPrinting: user?.name ?? 'Unknown',
-          projectName: project?.title,
-          purpose: project?.purpose,
-        },
-      ];
-    }
-  });
+    const stream = await renderToStream(<MultiPagePdf books={items} />);
+    const filename = project?.title
+      ? filenamify(`${project.title} - Pull Slips`)
+      : 'pullslips';
+    // logger.verbose('filename:', filename);
 
-  const stream = await renderToStream(<MultiPagePdf books={items} />);
-  const filename = project?.title
-    ? filenamify(`${project.title} - Pull Slips`)
-    : 'pullslips';
-  // logger.verbose('filename:', filename);
-
-  return new NextResponse(stream as any, {
-    headers: {
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': `inline; filename="${filename}.pdf"`,
-    },
-  });
+    return new NextResponse(stream as any, {
+      headers: {
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': `inline; filename="${filename}.pdf"`,
+      },
+    });
+  }
 }
