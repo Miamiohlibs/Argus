c2fe42812c1f356b54005fa87d883f447b2d47d2
Ken Irwin <ken@kenirwin.net>
2026-01-23 09:54:36 -0500

    require admin on admin/quickSlip

--- app/(main-layout)/admin/quickSlip/alma/page.tsx
+++ app/(main-layout)/admin/quickSlip/alma/page.tsx
@@ -1,6 +1,9 @@
 import RecordSearchForm from '@/components/RecordSearchForm';
+import checkAccess from '@/lib/checkAccess';
 
-export default function quickSlipAlmaPage() {
+export default async function quickSlipAlmaPage() {
+  await checkAccess({ permittedRoles: ['admin', 'superadmin'] });
+
   return (
     <>
       <h1>Lookup item for Quick Slip</h1>
