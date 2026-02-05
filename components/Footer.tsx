67cc58878f7dde8335d6cf9800f8bcac0c9f2339
Ken Irwin <ken@kenirwin.net>
2026-02-05 14:01:23 -0500

    update footer date, add repo link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
     <footer className="footer mt-auto py-1 bg-light text-center">
-      <p>© 2025 Argus version {process.env.APP_VERSION}</p>
+      <p>
+        © 2026 <Link href="https://github.com/Miamiohlibs/Argus">Argus</Link>{' '}
+        version {process.env.APP_VERSION}
+      </p>
     </footer>
   );
 };
