d55f5c34a607c18127108bdbf1b0906d442607fc
Ken Irwin <ken@kenirwin.net>
2026-03-02 11:54:14 -0500

    use slips render instead of slipsPdf api in ClientQuickPdf

--- app/(main-layout)/quickSlip/handler/ClientQuickPdf.tsx
+++ app/(main-layout)/quickSlip/handler/ClientQuickPdf.tsx
@@ -10,9 +10,9 @@ export default function ClientQuickPdf({ searchParams }: ClientQuickPdfProps) {
       Array.isArray(value)
         ? value.map((v) => [key, v])
         : value !== undefined
-        ? [[key, value]]
-        : []
-    )
+          ? [[key, value]]
+          : [],
+    ),
   ).toString();
 
   // console.log(`***** Query string in ClientPdf: ${queryString}`);
@@ -22,7 +22,7 @@ export default function ClientQuickPdf({ searchParams }: ClientQuickPdfProps) {
       <iframe
         src={`${
           process.env.NEXT_PUBLIC_APP_BASEPATH ?? ''
-        }/api/slipsPdf/quickSlip?${queryString}`}
+        }/slipsRender/quickSlip?${queryString}`}
         title="Quick Slip"
         style={{ width: '100%', height: '100%', border: 'none' }}
       />
