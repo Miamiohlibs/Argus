96ed77ac94abe8badc9b206c8e478cbbf7e28f64
Ken Irwin <ken@kenirwin.net>
2026-07-16 14:16:08 -0400

    handle WesternCollection "Range" call numbers in archivalObject.bib

--- lib/catalogs/aspace/callNumberOverrides.ts
+++ lib/catalogs/aspace/callNumberOverrides.ts
@@ -3,14 +3,18 @@ import type {
   RepoResources,
   RepoTopContainer,
 } from '@kenxirwin/archives-space-api-client';
+import { abort } from 'process';
 
 type ResourceInstance = RepoResources['instances'][number];
 type ArchivalObjectInstance = RepoArchivalObject['instances'][number];
 
 const callRegexMost = /\d+(A|M)\-[A-Z]\-\d+[A-Z]/g;
 // const callRegexRegGlobal = /\d+(A|M)\-[A-Z]\-\d+[A-Z]/g;
+//const callRegexWestern = /\[Range \d+[A-Z]\];* Box \d+/g;
 const callRegexWestern = /\[Range \d+[A-Z]\];* Box \d+/g;
 // const callRegexWesternGlobal = /\[Range \d+[A-Z]\];* Box \d+/g;
+
+const westernAncestorRef = '/repositories/2/resources/4';
 
 export interface AspaceCallNumberOverrides {
   resources?: {
@@ -36,21 +40,47 @@ export interface AspaceCallNumberOverrides {
  * implement whichever bib/item functions you need. See README.md for how
  * to set this file up so local edits don't conflict with upstream updates.
  */
+
+const condenseItemRange = (items: (string | undefined)[]) => {
+  const length = items.length;
+  const first = items[0];
+  const last = items[length - 1];
+  return `${first} ... ${last} (${length} items)`;
+};
+
 const overrides: AspaceCallNumberOverrides = {
   resources: {
     bib(data) {
       const itemCallNumbers = data.instances.map(
         (item) => item.sub_container.top_container._resolved?.display_string,
       );
-      const length = itemCallNumbers.length;
-      const first = itemCallNumbers[0];
-      const last = itemCallNumbers[length - 1];
-
-      return `${first} ... ${last} (${length} items)`;
+      return condenseItemRange(itemCallNumbers);
     },
   },
   archivalObject: {
     bib(data) {
+      const isWesternCollection =
+        data.ancestors.find((ancestor) => ancestor.ref == westernAncestorRef)
+          ?.ref == westernAncestorRef;
+
+      if (isWesternCollection) {
+        const abstractNotes = data.notes.filter(
+          (
+            note,
+          ): note is Extract<
+            (typeof data.notes)[number],
+            { jsonmodel_type: 'note_singlepart' }
+          > =>
+            note.type == 'abstract' &&
+            note.jsonmodel_type == 'note_singlepart' &&
+            note.content.length > 0,
+        );
+        const abstractString = abstractNotes
+          .flatMap((note) => note.content)
+          .join(' ');
+        const matches = abstractString.match(callRegexWestern) || [];
+        return condenseItemRange(matches) ?? '';
+      }
       const displayString = data.instances
         .map(
           (instance) =>
