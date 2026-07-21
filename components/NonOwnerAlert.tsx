f7caef4f692ffe4277bb5933335258e17c1b6994
Ken Irwin <ken@kenirwin.net>
2026-07-21 17:17:53 -0400

    inline icon for nonOwnerAlert

--- components/NonOwnerAlert.tsx
+++ components/NonOwnerAlert.tsx
@@ -3,9 +3,9 @@ import { ExclamationTriangle } from 'react-bootstrap-icons';
 const NonOwnerAlert = () => {
   return (
     <Alert variant="warning">
-      <ExclamationTriangle /> You have admin edit permissions, but you are not
-      the owner or co-editor of this project. With great power comes great
-      responsibility!
+      <ExclamationTriangle className="inline" /> You have admin edit
+      permissions, but you are not the owner or co-editor of this project. With
+      great power comes great responsibility!
     </Alert>
   );
 };
