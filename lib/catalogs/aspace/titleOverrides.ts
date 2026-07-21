6b0f6833bd38231feba9481076f6c3de9489a6d3
Ken Irwin <ken@kenirwin.net>
2026-07-21 08:52:59 -0400

    tidied titleOverrides

--- lib/catalogs/aspace/titleOverrides.ts
+++ lib/catalogs/aspace/titleOverrides.ts
@@ -3,30 +3,14 @@ import type {
   RepoResources,
   RepoTopContainer,
 } from '@kenxirwin/archives-space-api-client';
-import { ArchivalObjectExtraInfo } from '@/app/actions/aspaceSearch';
-
-// type ResourceInstance = RepoResources['instances'][number];
-// type ArchivalObjectInstance = RepoArchivalObject['instances'][number];
+import logger from '@/lib/logger';
 
 export interface AspaceTitleOverrides {
   resources?: (data: RepoResources) => string;
-  //   {
-  //     bib?:
-  //     // item?: (item: ResourceInstance, data: RepoResources) => string;
-  //   };
+
   topContainer?: (data: RepoTopContainer) => string;
-  //   {
-  //     bib?:
-  //     // item?: (data: RepoTopContainer) => string;
-  //   };
+
   archivalObject?: (data: RepoArchivalObject) => string;
-  //   {
-  //     bib?: (
-  //       ,
-  //       extraInfo?: ArchivalObjectExtraInfo,
-  //     ) => string;
-  // item?: (item: ArchivalObjectInstance, data: RepoArchivalObject) => string;
-  // allItems?: (data: RepoArchivalObject) => string[];
 }
 
 /**
@@ -53,7 +37,7 @@ const titleOverrides: AspaceTitleOverrides = {
   },
 
   topContainer: (data: RepoTopContainer): string => {
-    console.log('****trying to replace title string');
+    logger.silly('****trying to replace title string');
     if (data.long_display_string) {
       return streamlineTitle(data.long_display_string);
     } else {
