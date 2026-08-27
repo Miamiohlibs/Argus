0b7ab0851c48c100b0769c4e80aa6139450ca5f7
Ken Irwin <ken@kenirwin.net>
2026-08-27 13:28:57 -0400

    reinstate local miami changes

--- components/RequestSlips/BibSectionOverrides.tsx
+++ components/RequestSlips/BibSectionOverrides.tsx
@@ -1,18 +1,3 @@
-/* 
-  The page components returned by this file override the default
-  layouts for the printable request slips defined in:
-  - components/request/BibSection-default
-  - components/request/BibSection-aspace
-
-  This file is customizable when you fork the Argus repo for local use.
-  Changes you make to this file will not be overwritten by future software
-  updates. 
-  
-  To get started, you may wish to copy and paste the default code used in the
-  files referenced above and make any edits you desire to those starting 
-  layouts.
-*/
-
 import { RequestSlipProps } from '@/types/RequestSlipProps';
 import styles from './RequestSlipHalfPageHtml.module.css';
 import { shortenString } from './RequestSlipHalfPageHtml';
